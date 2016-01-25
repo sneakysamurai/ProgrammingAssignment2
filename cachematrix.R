247498051516e92abd5bde8f89e92409f7e2508f
sneakysamuri <kevin.simms649@gmail.com>
2016-01-24 19:18:45 -0500

    message again

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (original = matrix()){
+  
+  evildoppleganger <- matrix(nrow = nrow(original), ncol = ncol(original))
+  
+  set <- function (redo){
+    
+    original <<- redo
+    evildoppleganger <<- matrix(nrow = nrow(original), ncol(original))
+  }
+  
+  get <- function() {
+    original
+  }
+  
+  setinverse <- function (inverse) {
+    evildoppleganger <<- inverse
+  }
+  
+  getinverse <- function (){
+    evildoppleganger
+  }
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cachesolve <- function(newmatrix, ...){
+  
+  inverse <- newmatrix$getinverse()
+  
+  if(!is.na(inverse[1,1])){
+    ## had to look at a variable to see if it was NA instead of the entire matrix
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  else{
+    data <- newmatrix$get()
+    inverse <- solve(data)
+    newmatrix$setinverse(inverse)
+    inverse 
+  }
 }
